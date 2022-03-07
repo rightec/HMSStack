148ff2f49c4f59518c72f58dcc5534de68dc42e0
Fernando Morani <morani.rightec@gmail.com>
2022-03-07 22:12:34 +0100

    tag_0703_00

--- example_app/appl_adimap_tecna_ACYC.c
+++ example_app/appl_adimap_tecna_ACYC.c
@@ -33,6 +33,9 @@
 */
 
 #include "appl_adi_config.h"
+#include "abcc.h"                            // @tag_0703_01
+#include "../abcc_drv/src/abcc_debug_err.h"  // @tag_0703_01
+
 
 #if ( APPL_ACTIVE_ADI_SETUP == APPL_ADI_SETUP_TECNA_ACYC )
 
@@ -87,8 +90,8 @@ static void GetAdi12Value(const struct AD_AdiEntry* psAdiEntry, UINT8 bNumElemen
 */
 static UINT16  appl_aiUint16_10[ 32 ];
 static UINT16  appl_aiUint16_11[ 32 ];
-static UINT16  appl_Uint16_12 = 0;
-static UINT16  appl_glob_getCounter_12 = 0; //  @tag_tag_0703_00
+static UINT16  appl_Uint16_12 = 9;  // StartValue @tag_0703_01
+static UINT16  appl_glob_getCounter_12 = 0; //  @tag_0703_00
 /*******************************************************************************
 ** Public Globals
 ********************************************************************************
@@ -107,7 +110,8 @@ const AD_AdiEntryType APPL_asAdiEntryList[] =
 {
    { 10,   "ABP_UINT16_SET",        ABP_UINT16,   32, APPL_READ_MAP_WRITE_ACCESS_DESC, { { appl_aiUint16_10 ,NULL } }, NULL, SetAdi10Value },
    { 11,   "ABP_UINT16_GET",        ABP_UINT16,   32, APPL_WRITE_MAP_READ_ACCESS_DESC, { { appl_aiUint16_11 ,NULL } }, GetAdi11Value, NULL },
-   { 12,   "ABP_UINT16_COUNTER",    ABP_UINT16,   1,  APPL_NOT_MAP_WRITE_ACCESS_DESC,  { { &appl_Uint16_12 ,NULL  } }, GetAdi12Value, NULL         }, //  @tag_tag_0703_00
+   { 12,   "ABP_UINT16_COUNTER",    ABP_UINT16,   1,  APPL_WRITE_MAP_READ_ACCESS_DESC,  { { &appl_Uint16_12 ,NULL  } }, GetAdi12Value, NULL         }, //  @tag_tag_0703_00
+//   { 12,   "ABP_UINT16_COUNTER",    ABP_UINT16,   1,  APPL_NOT_MAP_WRITE_ACCESS_DESC,  { { &appl_Uint16_12 ,NULL  } }, GetAdi12Value, NULL         }, //  @tag_tag_0703_00
 };
 
 /*
@@ -167,6 +171,8 @@ static void SetAdi10Value( const struct AD_AdiEntry* psAdiEntry, UINT8 bNumEleme
 static void GetAdi12Value(const struct AD_AdiEntry* psAdiEntry, UINT8 bNumElements, UINT8 bStartIndex)
 {
     appl_glob_getCounter_12++;
+    DEBUG_EVENT(("appl_glob_getCounter_12 is now %d\n", appl_glob_getCounter_12)); // @tag_0703_01
+    DEBUG_EVENT(("ABCC_AnbState() is now %d\n", ABCC_AnbState())); // @tag_0703_01
     UINT8 index;
     for (index = bStartIndex; index < bStartIndex + bNumElements; index++)
     {
@@ -186,11 +192,29 @@ UINT16 APPL_GetNumAdi( void )
 
 void APPL_CyclicalProcessing( void )
 {
-   /*
-   ** This function is called when read and write data have been updated. It
-   ** could for example be used for operations on the ADI data.
-   ** Not used in this example.
-   */
+
+    if (ABCC_AnbState() == ABP_ANB_STATE_PROCESS_ACTIVE)
+    {
+        DEBUG_EVENT(("ABCC_AnbState() == ABP_ANB_STATE_PROCESS_ACTIVE\n")); // @tag_0703_01
+
+        /*
+        ** An example of ADI data handling.
+        */
+        if (appl_glob_getCounter_12 > 0 )
+        {
+            DEBUG_EVENT(("appl_glob_getCounter_12 is now %d\n", appl_glob_getCounter_12)); // @tag_0703_01
+
+        }
+        else 
+        {
+            /// 
+        }
+    }
+    else
+    {
+        /// 
+    }
+
 }
 
 /*******************************************************************************
